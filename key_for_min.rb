db0f2fefeb4a0a257b5cd99020fa26bf448033b9
Milan Winter <milanwinter8@gmail.com>
2020-08-03 06:52:17 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  min = 10000
+  name_hash.each do |key, value|
+    if min < value
+      min = value
+    end
+  end
 end
