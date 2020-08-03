82cc27b149c4c24faa7037d45dcd3b4302bb7f6e
Milan Winter <milanwinter8@gmail.com>
2020-08-03 07:28:58 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,9 +3,12 @@
 
 def key_for_min_value(name_hash)
   mininum = 10000
+  new_key = nil
   name_hash.each do |key, value|
     if mininum > value
       mininum = value
+      new_key = key
     end
   end
+  return new_key
 end
